72951f92cb7039b2457efadde056de0bd8bc250d
Женя Остапенко <jenya2003a@gmail.com>
2023-11-27 16:05:15 +0200

    Добавьте файлы проекта.

---
+++ Homework4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace Homework4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
